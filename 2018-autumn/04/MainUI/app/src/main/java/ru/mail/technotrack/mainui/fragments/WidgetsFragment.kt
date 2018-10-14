e09c5784095d4cbf36c81d982e6b1e805a82d681
Yury Bereza <ybereza@gmail.com>
2018-10-14 21:49:43 +0300

    Added widgets layout

--- 2018-autumn/04/MainUI/app/src/main/java/ru/mail/technotrack/mainui/fragments/WidgetsFragment.kt
+++ 2018-autumn/04/MainUI/app/src/main/java/ru/mail/technotrack/mainui/fragments/WidgetsFragment.kt
@@ -1,7 +1,14 @@
 package ru.mail.technotrack.mainui.fragments
 
+import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import ru.mail.technotrack.mainui.R
 
 class WidgetsFragment : Fragment() {
-
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.widgets, null)
+    }
 }
